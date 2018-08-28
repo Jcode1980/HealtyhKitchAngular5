e05f242377bb25a0308a1ac01c143f767d42ae50
John Adolfo <>
2018-08-28 12:29:13 +1000

    more changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,6 +41,7 @@ export class HomeComponent implements OnInit {
   showResultsBlock = false;
 
   private baseUrl = `${environment.apiUrl}api/recipes`;
+  public resourceURL = '${environment.apiUrl}';
 
   constructor(private homeService: HomeService, private http: HttpClient, private rest: RestService) {
   }
